f3dc229d8539ea6953c9b8f5dbe38247a390a697
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-11-30 19:52:17 +0100

    listing the menu on the user side

---
+++ Foodie.WebUI/ViewComponents/_PartialMenuCategory.cs
@@ -0,0 +1,30 @@
+﻿using Foodie.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Foodie.WebUI.ViewComponents
+{
+    public class _PartialMenuCategory : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PartialMenuCategory(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7285/api/Categories/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(categories);
+            }
+            return View();
+        }
+    }
+}
---
+++ Foodie.WebUI/ViewComponents/_PartialProductList.cs
@@ -0,0 +1,30 @@
+﻿using Foodie.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Foodie.WebUI.ViewComponents
+{
+    public class _PartialProductList : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PartialProductList(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7285/api/Products/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(products);
+            }
+            return View();
+        }
+    }
+}
