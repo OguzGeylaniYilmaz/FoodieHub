16851b30c1131a9cd17524c178e55cf049e7217d
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2026-01-01 21:11:19 +0100

    backend for reservation api and ui

--- Foodie.WebUI/Controllers/ReservationController.cs
+++ Foodie.WebUI/Controllers/ReservationController.cs
@@ -1,5 +1,6 @@
 ﻿using Foodie.WebUI.Dtos.ReservationDtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Foodie.WebUI.Controllers
 {
@@ -25,6 +26,71 @@ namespace Foodie.WebUI.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult CreateReservation()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation(CreateReservationDto createReservationDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createReservationDto);
+                var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("https://localhost:7285/api/Reservations", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("ReservationList");
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteReservation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7285/api/Reservations?id={id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ReservationList");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateReservation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7285/api/Reservations/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var reservation = JsonConvert.DeserializeObject<GetReservationByIdDto>(jsonData);
+                return View(reservation);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReservation(UpdateReservationDto updateReservationDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(updateReservationDto);
+                var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                var response = await client.PutAsync("https://localhost:7285/api/Reservations", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("ReservationList");
+                }
+            }
+            return View();
+
+        }
     }
 }
