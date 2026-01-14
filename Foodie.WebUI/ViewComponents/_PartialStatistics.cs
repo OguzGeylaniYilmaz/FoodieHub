c65a137bb8adf5db0f2e35d2cdeff0cd32d76e28
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2026-01-15 00:16:30 +0100

    updates on products and categories

---
+++ Foodie.WebUI/ViewComponents/_PartialStatistics.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.WebUI.ViewComponents
+{
+    public class _PartialStatistics : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PartialStatistics(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clientProductCount = _httpClientFactory.CreateClient();
+            var response = await clientProductCount.GetAsync("https://localhost:7285/api/Statistics/ProductCount");
+            var productCount = await response.Content.ReadAsStringAsync();
+            ViewBag.productCount = productCount;
+
+            var reservationClient = _httpClientFactory.CreateClient();
+            var reservationResponse = await reservationClient.GetAsync("https://localhost:7285/api/Statistics/ReservationCount");
+            var reservationCount = await reservationResponse.Content.ReadAsStringAsync();
+            ViewBag.reservationCount = reservationCount;
+
+            var chefClient = _httpClientFactory.CreateClient();
+            var chefResponse = await chefClient.GetAsync("https://localhost:7285/api/Statistics/ChefCount");
+            var chefCount = await chefResponse.Content.ReadAsStringAsync();
+            ViewBag.chefCount = chefCount;
+
+            var categoryClient = _httpClientFactory.CreateClient();
+            var categoryResponse = await categoryClient.GetAsync("https://localhost:7285/api/Statistics/CategoryCount");
+            var categoryCount = await categoryResponse.Content.ReadAsStringAsync();
+            ViewBag.categoryCount = categoryCount;
+            return View();
+        }
+    }
+}
---
+++ FoodieHub.API/Controllers/StatisticsController.cs
@@ -0,0 +1,45 @@
+﻿using FoodieHub.API.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodieHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public StatisticsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("ProductCount")]
+        public IActionResult GetProductCount()
+        {
+            var productCount = _context.Products.Count();
+            return Ok(productCount);
+        }
+
+        [HttpGet("ReservationCount")]
+        public IActionResult GetReservationCount()
+        {
+            var reservationCount = _context.Reservations.Count();
+            return Ok(reservationCount);
+        }
+
+        [HttpGet("ChefCount")]
+        public IActionResult GetChefCount()
+        {
+            var chefCount = _context.Chefs.Count();
+            return Ok(chefCount);
+        }
+
+        [HttpGet("CategoryCount")]
+        public IActionResult GetTotalCustomerCount()
+        {
+            var categoryCount = _context.Categories.Count();
+            return Ok(categoryCount);
+        }
+    }
+}
